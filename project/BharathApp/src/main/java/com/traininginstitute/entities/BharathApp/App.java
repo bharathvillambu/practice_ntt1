347c473a0bf0ecde7c12c3733e6e24c362bf2df5
bharathvillambu <91466166+bharathvillambu@users.noreply.github.com>
2021-12-08 15:30:51 +0530

    Add files via upload

---
+++ project/BharathApp/src/main/java/com/traininginstitute/entities/BharathApp/App.java
@@ -0,0 +1,13 @@
+package com.traininginstitute.entities.BharathApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bharath!" );
+    }
+}
